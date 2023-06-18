c66964f6da73996bb28c064821d36b5d47dae9a5
mannurajput <110715475+mannurajput@users.noreply.github.com>
2023-06-19 00:53:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Clubs from "./Components/Clubs";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Clubs />
+    </div>
+  );
+}
+
+export default App;
